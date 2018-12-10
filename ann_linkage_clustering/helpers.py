75a9fba91548e8d782044adb0d8ec496fcc4fd38
Sam Minot <sam@fredhutch.org>
2018-12-11 04:28:08 +0800

    Equality statement evaluates correctly in this case

--- ann_linkage_clustering/helpers.py
+++ ann_linkage_clustering/helpers.py
@@ -185,7 +185,7 @@ def normalize_abundance_dataframe(df, normalization):
             df.isnull().any(axis=1),
             df.isnull().any(axis=0)
         ].to_string())
-    assert df.isnull().any().any() is False
+    assert df.isnull().any().any() == False
 
     return df
 
